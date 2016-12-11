d296225439509096e13dac79d5568b437cb18f4a
andr-andreev <andr-andreev@users.noreply.github.com>
2016-12-11 22:23:12 +0300

    Using Dependency Injection

--- web/index.php
+++ web/index.php
@@ -3,19 +3,33 @@ declare(strict_types = 1);
 
 namespace SocialRss;
 
+use Slim\App;
+use Slim\Container;
 use Slim\Http\Request;
 use Slim\Http\Response;
-use SocialRss\Parser\Parser;
-use SocialRss\Format\Format;
 use SocialRss\Exception\SocialRssException;
+use SocialRss\Format\FormatFactory;
+use SocialRss\Format\FormatInterface;
+use SocialRss\Parser\ParserFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new \Slim\App([
-    'settings' => [
-        'displayErrorDetails' => true,
-    ]
-]);
+$config = parse_ini_file("../.env", true, INI_SCANNER_TYPED);
+
+$app = new App(['settings' => $config]);
+
+// Retrieve container instance
+$container = $app->getContainer();
+
+// Register parserFactory
+$container['parserFactory'] = function (Container $c) {
+    return new ParserFactory();
+};
+
+// Register formatFactory
+$container['formatFactory'] = function (Container $c) {
+    return new FormatFactory();
+};
 
 $app->get('/{source}', function (Request $request, Response $response) {
     $source = $request->getAttribute('source');
@@ -24,14 +38,17 @@ $app->get('/{source}', function (Request $request, Response $response) {
     $username = $params['username'] ?? '';
     $output = $params['output'] ?? 'rss';
 
-    $config = parse_ini_file("../.env", true);
+    $config = $this->get('settings');
 
     if (!isset($config[$source])) {
         throw new SocialRssException("No config found for $source source");
     }
 
-    $parser = new Parser($source, $config[$source]);
-    $writer = new Format($output);
+    /* @var $parser \SocialRss\Parser\ParserInterface */
+    $parser = $this->get('parserFactory')->create($source, $config[$source]);
+
+    /* @var $writer FormatInterface */
+    $writer = $this->get('formatFactory')->create($output);
 
     $feed = $parser->getFeed($username);
     $parsedFeed = $parser->parseFeed($feed);
